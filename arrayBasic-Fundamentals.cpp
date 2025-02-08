29cbf7e3383b3e26b4c9cbdceea38ec0d9e069cb
Katherine Anne <katherine_anne_liwanag@dlsl.edu.ph>
2025-02-08 22:07:29 +0800

    Comments about program

---
+++ arrayBasic-Fundamentals.cpp
@@ -0,0 +1,12 @@
+//Author: Katherine Anne S. Liwanag
+
+/* This is a program that ask users to input elements for two arrays which are
+re-arrange to descending order.*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
